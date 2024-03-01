dc2d96301eabbaa28c463731268581839898cad5
publicgithub69 <160835075+publicgithub69@users.noreply.github.com>
2024-03-01 14:09:24 +0530

    Update read.py

--- read.py
+++ read.py
@@ -1,18 +1,29 @@
 import cv2 as cv
 
+def rescaleFrame(frame,scale=0.75):
+    width = int(frame.shape[1] * scale)
+    height = int(frame.shape[0] * scale)
+    dimensions = (width,height)
+
+    return cv.resize(frame,dimensions, interpolation=cv.INTER_AREA)
 
 # Reading image
-#img = cv.imread('images/dog_1.webp')
-#cv.imshow('dog',img)
+img = cv.imread('images/dog_1.webp')
+cv.imshow('dog',rescaleFrame(img))
 
 #Reading Video
-capture=cv.VideoCapture('videos/dog_video.mp4')
+#capture=cv.VideoCapture('videos/dog_video.mp4')
 
 
+    
+'''
 while True:
     isTrue, frame = capture.read()
     cv.imshow('video',frame)
     if cv.waitKey(20) & 0xFF==ord('d'):
         break
-capture.release()
+'''
+
+
+#capture.release()
 cv.destroyAllWindows()
